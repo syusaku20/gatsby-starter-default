d8d56579937357014d860343c62e7f74efe86b59
syusaku20 <51801920+syusaku20@users.noreply.github.com>
2019-08-03 13:00:57 +0900

    22s
    変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,18 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>おはよう</h1>
+    <h1>hello world</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </p>
+    <p>
+      <Link to="/about/">Go to about</Link>
+    </p>
   </Layout>
 )
 
